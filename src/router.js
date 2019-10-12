3aff0ab36af23fce510ce855f2e4abdb81769165
AdamSiekierski <adam.siekiera@outlook.com>
2019-10-12 08:27:17 +0200

    Fix router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Overview.vue';
+import Manage from './views/Manage.vue';
 
 Vue.use(Router);
 
@@ -16,10 +17,7 @@ export default new Router({
     {
       path: '/manage',
       name: 'Manage',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "manage" */ './views/Manage.vue'),
+      component: Manage,
     },
   ],
 });
