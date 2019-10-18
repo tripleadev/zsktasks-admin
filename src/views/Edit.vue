5b3ab5d7288ec2fa5116136bbf2a05a478424053
Adam Siekierski <adam.siekiera@outlook.com>
2019-10-18 21:37:25 +0200

    Add edit.vue file

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,10 +1,14 @@
-<template></template>
+<template>
+  <div>
+    Edit
+  </div>
+</template>
 <script>
 export default {
   name: "Edit",
   props: {
     id: String,
-  }
+  },
 };
 </script>
 <style lang="scss" scoped>
